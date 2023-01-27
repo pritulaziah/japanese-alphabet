7290ad9ebb75b19279666e4d081e3b43926a4c2f
pritulaziah <ziahherzen@gmail.com>
2023-01-27 05:14:17 +0500

    update examples

--- components/alphabet/KanaTable/CharacterContent.tsx
+++ components/alphabet/KanaTable/CharacterContent.tsx
@@ -3,12 +3,14 @@ import {
   AlphabetTypes,
   AlphabetForms,
 } from "types/alphabet";
+import { getAPIWords } from "pages/api/words";
+import { IWord } from "types/word";
 import getAlphabetTypeStyles from "utils/getAlphabetTypeStyles";
 import Modal from "components/common/Modal";
 import React, { useEffect, useState } from "react";
 import Spinner from "components/common/Spinner";
 
-const getHighlightChar = (
+const getStrWithHighlightChar = (
   japaneseStr: string,
   currentChar: string,
   type: AlphabetTypes
@@ -41,26 +43,53 @@ interface IProps {
 
 const CharacterContent = ({ character, form }: IProps) => {
   const currentFormCharacter = character[form];
-  const [icon, setIcon] = useState<null | {
-    default: React.ComponentType;
-  }>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [iconInfo, setIconInfo] = useState<{
+    icon: { default: React.ComponentType } | null;
+    isLoading: boolean;
+  }>({ isLoading: false, icon: null });
+  const [examplesData, setExamplesData] = useState<{
+    examples: null | IWord[];
+    isLoading: boolean;
+  }>({
+    examples: null,
+    isLoading: false,
+  });
 
   useEffect(() => {
     const loadKanjiIcon = async () => {
       if (currentFormCharacter.length === 1) {
-        setIsLoading(true);
+        setIconInfo((prevState) => ({ ...prevState, isLoading: true }));
         const characterIcon = await import(
           `kanji-react-icons/dist/${form}/${currentFormCharacter}.js`
         );
 
-        characterIcon && setIcon(characterIcon);
-        setIsLoading(false);
+        setIconInfo({
+          icon: characterIcon,
+          isLoading: false,
+        });
       }
     };
 
     loadKanjiIcon();
   }, [form, currentFormCharacter]);
+
+  useEffect(() => {
+    const getExamples = async () => {
+      setExamplesData((prevState) => ({ ...prevState, isLoading: true }));
+      const response = await getAPIWords({
+        search: currentFormCharacter,
+        offset: 0,
+        limit: 3,
+      });
+
+      setExamplesData({
+        examples: response.data.data,
+        isLoading: false,
+      });
+    };
+
+    getExamples();
+  }, []);
 
   return (
     <>
@@ -86,49 +115,56 @@ const CharacterContent = ({ character, form }: IProps) => {
             </span>
           </div>
         </div>
-        {isLoading ? (
+        {iconInfo.isLoading ? (
           <Spinner size="md" />
         ) : (
-          icon?.default && (
+          iconInfo.icon?.default && (
             <div className="w-max h-64 dark:[&_path]:!stroke-white dark:hover:[&_path]:!stroke-red-500 hover:[&_path]:!stroke-red-600 [&_path]:transition-[stroke] [&_path]:duration-250 cursor-pointer">
-              <icon.default />
+              <iconInfo.icon.default />
             </div>
           )
         )}
-        {/* {currentForm.examples.length > 0 && (
-          <>
-            <h3 className="mb-1.5 text-md text-neutral-700 dark:text-neutral-200">
-              Примеры:{" "}
-            </h3>
-            <ul className="list-none">
-              {currentForm.examples.map((example, index) => (
-                <li key={index} className="mb-4 last-of-type:mb-0">
-                  <div className="flex">
-                    <span className="mr-2 text-neutral-400">{index + 1}.</span>
-                    <div className="flex flex-col">
-                      <span
-                        lang="ja"
-                        className="font-japanese text-xl text-neutral-900 dark:text-neutral-50 mb-1"
-                      >
-                        {getHighlightChar(
-                          example.japanese,
-                          currentForm.character,
-                          character.type
-                        )}
+        {examplesData.isLoading ? (
+          <Spinner size="md" />
+        ) : (
+          examplesData.examples != null &&
+          examplesData.examples.length > 0 && (
+            <>
+              <h3 className="mb-1.5 text-md text-neutral-700 dark:text-neutral-200">
+                Примеры:{" "}
+              </h3>
+              <ul className="list-none">
+                {examplesData.examples.map((example, index) => (
+                  <li key={index} className="mb-4 last-of-type:mb-0">
+                    <div className="flex">
+                      <span className="mr-2 text-neutral-400">
+                        {index + 1}.
                       </span>
-                      <span className="text-neutral-600 dark:text-neutral-200">
-                        {example.romaji}
-                      </span>
-                      <span className="text-neutral-500 dark:text-neutral-400 text-sm">
-                        {example.meaning}
-                      </span>
+                      <div className="flex flex-col">
+                        <span
+                          lang="ja"
+                          className="font-japanese text-xl text-neutral-900 dark:text-neutral-50 mb-1"
+                        >
+                          {getStrWithHighlightChar(
+                            example.japanese,
+                            currentFormCharacter,
+                            character.type
+                          )}
+                        </span>
+                        <span className="text-neutral-600 dark:text-neutral-200">
+                          {example.romaji}
+                        </span>
+                        <span className="text-neutral-500 dark:text-neutral-400 text-sm">
+                          {example.meaning}
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </>
-        )} */}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )
+        )}
       </Modal.Body>
     </>
   );
